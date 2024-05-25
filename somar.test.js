858fd2e933d3ca7990eef9d122d014e349629744
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-05-24 22:17:58 -0300

    Começando varios testes

---
+++ somar.test.js
@@ -0,0 +1,11 @@
+const somar = require("./somar")
+
+test('somar 1 + 2 é igual a 3', ()=>{
+    expect(somar(1,2)).toBe(3)
+})
+
+test('Soma de dois números inteiros, somar 3 + 5 = 8', ()=>{
+    expect(somar(3,5)).toBe(8)
+})
+
+test('')
