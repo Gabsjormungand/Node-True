9a828d98bc1c362758bdb9bed652e69d94d01105
Gabriel Berserk <143458446+Gabsjormungand@users.noreply.github.com>
2024-05-27 21:14:37 -0300

    Atividades

--- somar.js
+++ somar.js
@@ -1,5 +1,10 @@
 function somar(a,b){
+    if((typeof a != 'number') || (typeof b !=  'number')){
+        return "Caractere inválido"
+    }
     return a+b
 }
 
+
 module.exports = somar
+
--- somar.test.js
+++ somar.test.js
@@ -4,8 +4,42 @@ test('somar 1 + 2 é igual a 3', ()=>{
     expect(somar(1,2)).toBe(3)
 })
 
+// Caso de Teste 1
 test('Soma de dois números inteiros, somar 3 + 5 = 8', ()=>{
     expect(somar(3,5)).toBe(8)
 })
 
-test('')
+// Caso de Teste 2
+test('Somar dois número negativo, somar -2 + -5 = -8', ()=>{
+    expect(somar(-3,-5)).toBe(-8)
+})
+
+// Caso de Teste 3
+test('Somar/Substrair de número positivo e negativo, 7 - -2 = 5', ()=>{
+    expect(somar(7,-2)).toBe(5)
+})
+
+// Caso de Teste 4
+test('Somar o número 0 com umk outro número, somar 0 + 9 = 9',()=>{
+    expect(somar(0,9)).toBe(9)
+})
+
+// Caso de Teste 5
+test('Somar dois números decimais, somar 2,5 + 3.1 = 5.6', ()=>{
+    expect(somar(2.5,3.1)).toBe(5.6)
+})
+
+// Caso de Teste 6
+test('Somar um número inteiro com um número decimal, somar 4 + 2.5 = 6.5', ()=>{
+    expect(somar(4,2.5)).toBe(6.5)
+})
+
+// Caso de Teste 7
+test('Verificação de entrada inválida, A + 3 = Caractere Inválido', ()=>{
+    expect(somar("A",3)).toBe('Caractere inválido')
+})
+
+// Caso de Teste 8
+test('Soma de números grandes, 999999999 + 999999999 = 1999999998', ()=>{
+    expect(somar(999999999,999999999)).toBe(1999999998)
+})
